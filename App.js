174ce7b6ddcb15b38cd77a4be9e0b83cc8274f71
Bradley Wittenbrook <bradley.wittenbrook@gmail.com>
2018-05-06 15:03:09 -0400

    Add basic tab nav to show all decks and new deck screen

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
+import { TabNavigator } from 'react-navigation'
+import { Constants } from 'expo'
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
+
+import DecksScreen from './components/DecksScreen'
+import NewDeckScreen from './components/NewDeckScreen'
+
+const UdaciStatusBar = ({backgroundColor, ...props}) => (
+  <View style={{height: Constants.statusBarHeight }}>
+    <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+  </View>
+)
+
+const TabNav = TabNavigator({
+  Decks: {
+    screen: DecksScreen,
+    navigationOptions: {
+      title: `Decks`,
+      tabBarIcon: () => (
+        <MaterialCommunityIcons name="cards-outline" size={27}/>
+      )
+    },
+  },
+  NewDecks: {
+    screen: NewDeckScreen,
+    navigationOptions: {
+      title: `Add Deck`,
+      tabBarIcon: () => (
+        <Ionicons name="ios-add-circle" size={30} />
+      )
+    },
+  }
+});
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{flex: 1}}>
+        <UdaciStatusBar />
+        <TabNav />
       </View>
     );
   }
---
+++ components/DecksScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+
+export default ({}) => (
+  <View style={styles.container}>
+    <Text>DecksScreen</Text>
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
